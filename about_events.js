3a63cf3786b7d901a71122c0da04561ded3cbdbb
Jane Philipps <jane.philipps@gmail.com>
2015-08-23 18:08:00 -0700

    fix navbar alignment, add timeline css and js

---
+++ about_events.js
@@ -0,0 +1,92 @@
+(function( global ) {
+  "use strict";
+
+  global.aboutEvents = {
+
+    "mig" : {
+      "year" : "2013",
+      "description" : "MIG, Berkeley, CA<br/><strong>Graphic Designer</strong>, October 2013-Present<br/>"
+
+    },
+
+    "running2" : {
+      "year" : "2013",
+      "description" : "In October 2013, I finished my first full-marathon in Chicago, IL."
+
+    },
+
+    "dial-house" : {
+      "year" : "2012",
+      "description" : "Dial House, San Francisco, CA<br/><strong>Designer</strong>, March 2012-August 2013<br/>Dial House is a brand strategy firm with a multi-national client list, that represents recognizable products and innovations."
+
+    },
+
+    "running" : {
+      "year" : "2010",
+      "description" : "During the past few years, I have become an avid runner. In 2010, I finished my first half-marathon, the NYC Half."
+
+    },
+
+    "yj" : {
+      "year" : "2010",
+      "description" : "Active Interest Media, San Francisco, CA</br><strong>Designer</strong>, Yoga Journal Magazine Art Department, March 2010-March 2012"
+
+    },
+
+    "sf" : {
+      "year" : "2010",
+      "description" : "After much consideration, and with great excitement, I made the decision to move to San Francisco."
+
+    },
+
+    "oxford" : {
+      "year" : "2009",
+      "description" : "Oxford University Press, New York, NY<br/><strong>Part-time Design Assistant</strong>, Trade Art Department, September 2009- February 2010"
+
+    },
+
+    "columbia" : {
+      "year" : "2009",
+      "description" : "Columbia University,<br/>Graduate School of Journalism,<br/>Columbia Publishing Course,<br/>June-July 2009 <br/>Six-week intensive course on all aspects of book and magazine"
+
+    },
+
+    "wash" : {
+      "year" : "2009",
+      "description" : "Washington University </br>in St. Louis</br>Sam Fox School of Design & Visual Arts-BFA, May 2009</br>Communication Design Major, Architecture Minor"
+
+    },
+
+    "aiga" : {
+      "year" : "2009",
+      "description" : "AIGA 14 Show, St. Louis, MO, Student Merit Award, April 2009"
+
+    },
+
+    "newspaper" : {
+      "year" : "2008",
+      "description" : "Student Life Newspaper, </br>St. Louis, MO<br/><strong>Design Production Manager</strong>, Business and Advertising Department, January 2008-May 2009"
+
+    },
+
+    "rodale" : {
+      "year" : "2008",
+      "description" : "Rodale Publications, New York, NY<br/><strong>Art Intern</strong>, Women's Health Magazine Art Department, May 2008-August 2008"
+
+    },
+
+    "mtv" : {
+      "year" : "2007",
+      "description" : "MTV Networks, </br>New York, NY<br/><strong>Production Intern</strong>, VH1 On-Air Promotions (Viacom Intern Program), May 2007-August 2007"
+
+    },
+
+    "born" : {
+      "year" : "1987",
+      "description" : "I was born on Long Island in New York."
+
+    }
+
+  };
+
+})( window );
